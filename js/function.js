1a57d31e396a4ea0544166e9e85d4669047a0a1a
Nik_Laptop <NikGaydar@yandex.ru>
2024-08-18 12:35:22 +0700

    WIP

---
+++ js/function.js
@@ -0,0 +1,30 @@
+/**
+ * Описание функции по методу объявления function decloration
+ * @param {number} par1 - первый параметр 
+ * @param {number} par2 - второй параметр
+ * @returns {number} - тип данных возращаемых функцией
+ */
+
+function declorationFunc(par1, par2) {};
+
+// функция становится методом глобального объекта window
+// при объявлении функции по методу fuction decloration, функцию можно вызывать до ее объвления
+
+/**
+ * Описание функции по методу объявления function expression
+ * @param {number} par1 - первый параметр 
+ * @param {number} par2 - второй параметр
+ * @returns {number} - тип данных возращаемых функцией
+ */
+
+const expressionFunc(par1, par2) {};
+
+// Можно вызывать фуекцию только после ее определения
+
+/**
+ * Описание стрелочной функции 
+ * @param {number} par1 - первый параметр 
+ * @param {number} par2 - второй параметр
+ * @returns {number} - тип данных возращаемых функцией
+ */
+
