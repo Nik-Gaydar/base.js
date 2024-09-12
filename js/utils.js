ac3e6d3942e2004268f3f26f6a6ae5c62321db83
Nik-Gaydar <nik-gaydar@yandex.ru>
2024-09-12 12:17:53 +0700

    addition function with check

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,21 @@
 const calcSum = (num1, num2) => {
+  if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    return "Ошибка: ожидались числа в качестве входящих данных"
+  }
+  if (arguments.lenght !==2) {
+    return "функция ожидает два аргумента"
+  }
   return num1 + num2;
 };
 
-console.log(calcSum(1, 2));
+console.log(calcSum(0, 45, 4));
 
-console.log(calcSum(3, 4));
+// console.log(calcSum(3, 4));
+
+// console.log(calcSum(3, "Nikitos"));
+
+// console.log(calcSum(3, undefined));
+
+console.log(calcSum(3));
+
+// console.log(calcSum(3, NaN));
