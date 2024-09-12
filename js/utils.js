6e4a7056f751672bf0d80a96e17d2d8416a4f837
Nik_Gaydar <nikgaydar@yandex.ru>
2024-09-12 15:53:39 +0700

    Added function and checked

--- js/utils.js
+++ js/utils.js
@@ -1,8 +1,11 @@
-const calcSum = (num1, num2) => {
+const calcSum = function (num1, num2) {
+  if (num1 === undefined || num2 === undefined) {
+    return "красавчик!"
+  }
   if (typeof num1 !== 'number' || typeof num2 !== 'number') {
     return "Ошибка: ожидались числа в качестве входящих данных"
   }
-  if (arguments.lenght !==2) {
+  if (arguments.length !== 2) {
     return "функция ожидает два аргумента"
   }
   return num1 + num2;
@@ -10,12 +13,12 @@ const calcSum = (num1, num2) => {
 
 console.log(calcSum(0, 45, 4));
 
-// console.log(calcSum(3, 4));
+console.log(calcSum(3, 4));
 
-// console.log(calcSum(3, "Nikitos"));
+console.log(calcSum(3, "Nikitos"));
 
-// console.log(calcSum(3, undefined));
+console.log(calcSum(3, undefined));
 
 console.log(calcSum(3));
 
-// console.log(calcSum(3, NaN));
+console.log(calcSum(3, NaN));
