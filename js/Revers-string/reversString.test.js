ed3f17bdccb0433971bcfe288a40ccd7b58d7d1f
Nik_Gaydar <nikgaydar@yandex.ru>
2024-09-24 16:36:08 +0700

    Added reversString.test.js

---
+++ js/Revers-string/reversString.test.js
@@ -0,0 +1,11 @@
+import {reversString} from './reversString.js';
+
+console.log(reversString('word'));
+
+console.log(reversString('ежу хуже'));
+
+console.log(reversString('1234567'));
+
+// console.log(reversString(1234567));
+
+console.log(reversString(''));
