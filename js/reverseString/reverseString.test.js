3f41082da574ef95839d413d3223aaaf1405711a
Nik-Gaydar <nik-gaydar@yandex.ru>
2024-09-25 21:03:24 +0700

    Fixed the function name

---
+++ js/reverseString/reverseString.test.js
@@ -0,0 +1,11 @@
+import {reverseString} from './reverseString.js';
+
+console.log(reverseString('word'));
+
+console.log(reverseString('ежу хуже'));
+
+console.log(reverseString('1234567'));
+
+// console.log(reversString(1234567));
+
+console.log(reverseString(''));
