b5fbc731ac0fc088e35108d7ac757d2999329229
Nik_Gaydar <nikgaydar@yandex.ru>
2024-10-08 16:20:31 +0700

    Implemented testing for the function

--- js/reverseString/reverseString.test.js
+++ js/reverseString/reverseString.test.js
@@ -1,11 +1,34 @@
 import {reverseString} from './reverseString.js';
+import { expect, it } from '@jest/globals';
 
-console.log(reverseString('word'));
+// test (
+//   'checking the reverse of the string',
+//   () => {
+//     expect(reverseString('hello gyu')).toBe('uyg olleh');
+//   }
+// );
 
-console.log(reverseString('ежу хуже'));
-
-console.log(reverseString('1234567'));
-
-// console.log(reversString(1234567));
-
-console.log(reverseString(''));
+describe(
+  'checking the reverse of the string',
+  () => {
+    const testCase = [
+      {
+        inString: 'hello',
+        expected: 'olleh'
+      },
+      {
+        inString: 'guys',
+        expected: 'syug'
+      },
+    ];
+    testCase.forEach(test => {
+      it(
+        `Входящая строка: ${test.inString} ожидают: ${test.expected}`,
+        () => {
+          const res = reverseString(test.inString);
+          expect(res).toBe(test.expected);
+        }
+      );
+    });
+  }
+);
